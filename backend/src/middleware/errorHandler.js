b2ce9b46e445b1b985a9386b72845d7932659387
Nithya bikkanuru <nithyabikkanuru@gmail.com>
2026-03-09 19:38:46 +0530

    Update errorHandler.js

--- backend/src/middleware/errorHandler.js
+++ backend/src/middleware/errorHandler.js
@@ -1,7 +1,3 @@
-// module.exports = (err, req, res, next) => {
-//     console.error(err.stack);
-//     res.status(500).json({ message: err.message });
-//   };
 module.exports = (err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: err.message });
