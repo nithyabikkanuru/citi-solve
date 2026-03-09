7c5d22fa94e344f60c2d989c50c816892d3fe20a
Nithya bikkanuru <nithyabikkanuru@gmail.com>
2026-03-09 19:39:54 +0530

    Update complaintRoutes.js

--- backend/src/routes/complaintRoutes.js
+++ backend/src/routes/complaintRoutes.js
@@ -1,20 +1,3 @@
-// import express from "express";
-// import {
-//   createComplaint,
-//   getMyComplaints,
-//   getAllComplaints
-// } from "../controllers/complaintController.js";
-
-// import { protect } from "../middleware/auth.js";
-
-// const router = express.Router();
-
-// router.post("/", protect, createComplaint);
-// router.get("/my", protect, getMyComplaints);
-// router.get("/", protect, getAllComplaints);
-
-// export default router;
-
 import express from "express";
 import {
   createComplaint,
